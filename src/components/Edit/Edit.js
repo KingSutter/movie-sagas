f8ce3568d7f2aa8d22009501ebe2ed8ecf59592a
Will Sutter <kingsutter12@gmail.com>
2019-11-10 15:37:14 -0600

    set up form on edit page

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Edit extends Component {
+    state = {
+        titleIn: '',
+        descripionIn: '',
+    }
+    handleTitleChange = (event) => {
+        this.setState({
+            titleIn: event.target.value,
+        });
+    }
+    handleDescriptionChange = (event) => {
+        this.setState({
+            descripionIn: event.target.value,
+        });
+    }
+
     render(){
         return (
             <div>
-                test
+                <form onSubmit={this.handeSubmit}>
+                    <input onChange={this.handleTitleChange} type="text" placeholder="title"/>
+                    <input onChange={this.handleDescriptionChange} type="text" placeholder="description"/>
+                    <button type="submit">Submit</button>
+                </form>
+            <pre>{JSON.stringify(this.state,null,2)}</pre>
             </div>
         )
     }
 }
 
-export default Edit;
+const mapReduxStateToProps = (reduxState) => {
+    return {movie: reduxState.movieDetails};
+}
+
+export default connect(mapReduxStateToProps)(Edit);
